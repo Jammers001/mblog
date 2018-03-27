339c510a716dd9987d4db47105be592694802273
jammers001 <jammers_lee@foxmail.com>
2018-03-27 17:37:34 +0800

    ‘

--- mainsite/views.py
+++ mainsite/views.py
@@ -3,8 +3,10 @@
 from .models import Post
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from datetime import datetime
+from django.template.loader import get_template
 # Create your views here.
+'''
 def homepage(request):
     posts=Post.objects.all()
     post_lists=list()
@@ -13,3 +15,11 @@ def homepage(request):
         post_lists.append('<font size="3" color="blue">'+str(post.body)+
                           '</font><br><br>')
     return HttpResponse(post_lists)
+'''
+def homepage(request):
+    templates=get_template('index.html')
+    posts=Post.objects.all()
+    now=datetime.now()
+    html=templates.render(locals())
+    return HttpResponse(html)
+
